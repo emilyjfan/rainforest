90009edea4be98127724037dad800b30e9e6db09
emilyjfan <emilyjustinefan@gmail.com>
2015-02-09 14:11:52 -0500

    Creating new and create actions for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    resources :products
+  resources :products
+  resources :users, only: [:new, :create]
+end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -56,4 +58,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
